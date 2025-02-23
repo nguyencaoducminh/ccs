2ba8fc21787b1a3837f2acc9002db3eb12177c64
Nguyen Cao Duc Minh <nguyencaoducminh2711@gmail.com>
2025-02-23 13:22:36 +0700

    Add ability to load data from Meier 2021 Dataset

--- dataloaderccs.py
+++ dataloaderccs.py
@@ -13,7 +13,14 @@ Load CCS datasets
 
 Ionmob
 PATH: ./data/ionmod
-AVAILABLE: chang, ogata, sara, tenzer, tenzer-phospho, zepeda with 'all' for the combined dataset
+AVAILABLE: chang, ogata, sara, tenzer, tenzer-phospho, zepeda with 'ionmod' for the combined dataset
+
+Meier 2021
+PATH: ./data/meier_2021
+AVAILABLE:
+    - small : the first 100000 entries of SourceData_Figure_1.csv (80:20 train:val:test)
+    - train : all of SourceData_Figure_1.csv (80:20 train:val)
+    - meier : all Meier 2021 datasets with SourceData_Figure_1.csv (80:20 train:val) and SourceData_Figure_4.csv as test
 """
 
 # For git repos
@@ -31,7 +38,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 random = 1024
 
 # Available datasets
-IONMOD_DATASETS = ['chang', 'ogata', 'sara', 'tenzer', 'tenzer-phospho', 'zepeda', 'all']
+IONMOD_DATASETS = ['chang', 'ogata', 'sara', 'tenzer', 'tenzer-phospho', 'zepeda', 'ionmod']
+MEIER_DATASETS = ['small', 'train', 'meier']
 
 def min_max_scale(x, min, max):
     new_x = 1.0 * (x - min) / (max - min)
@@ -78,7 +86,7 @@ class data_ionmod():
             data_c1 = pd.read_parquet(IONMOD_DIR + '/Zepeda_unique_c1_unimod.parquet')
             data_thunder = pd.read_parquet(IONMOD_DIR + '/Zepeda_thunder_unique_unimod.parquet')
             data = pd.concat([data, data_c1, data_thunder])
-        elif dataset == 'all':
+        elif dataset == 'ionmod':
             data_chang = pd.read_parquet(IONMOD_DIR + '/Chang_unique_unimod.parquet')
             data_otega = pd.read_parquet(IONMOD_DIR + '/Ogata_unique_unimod.parquet')
             data_sara = pd.read_parquet(IONMOD_DIR + '/Sara_unique_unimod.parquet')
@@ -97,7 +105,7 @@ class data_ionmod():
             return cls.filter_data(data)
     
     @classmethod
-    def ionmod_to_integet(cls, sequences, max_sequence_length = 60):
+    def ionmod_to_integer(cls, sequences, max_sequence_length = 60):
         
         IONMOD_ALPHABET = {
             "A": 1,
@@ -162,9 +170,9 @@ class data_ionmod():
                 data_train, data_valid = cls.load_data('tenzer-phospho', ionmod_full)
             else:
                 data_train, data_valid = cls.load_data('tenzer-phospho')
-            x_train = cls.ionmod_to_integet(data_train['sequence-tokenized'], max_sequence_length = max_sequence_length)
+            x_train = cls.ionmod_to_integer(data_train['sequence-tokenized'], max_sequence_length = max_sequence_length)
             y_train = data_train['ccs'].to_numpy()
-            x_valid = cls.ionmod_to_integet(data_valid['sequence-tokenized'], max_sequence_length = max_sequence_length)
+            x_valid = cls.ionmod_to_integer(data_valid['sequence-tokenized'], max_sequence_length = max_sequence_length)
             y_valid = data_valid['ccs'].to_numpy()
             return (x_train, y_train), (x_valid, y_valid)
 
@@ -175,12 +183,12 @@ class data_ionmod():
         data_train_val, data_test = cls.split_data(data)
         data_train, data_val = cls.split_data(data_train_val, split=0.875)
         
-        x_train = cls.ionmod_to_integet(data_train['sequence-tokenized'], max_sequence_length = max_sequence_length)
+        x_train = cls.ionmod_to_integer(data_train['sequence-tokenized'], max_sequence_length = max_sequence_length)
         y_train = data_train['ccs'].to_numpy()
-        x_val = cls.ionmod_to_integet(data_val['sequence-tokenized'], max_sequence_length = max_sequence_length)
+        x_val = cls.ionmod_to_integer(data_val['sequence-tokenized'], max_sequence_length = max_sequence_length)
         y_val = data_val['ccs'].to_numpy()
         
-        x_test = cls.ionmod_to_integet(data_test['sequence-tokenized'], max_sequence_length = max_sequence_length)
+        x_test = cls.ionmod_to_integer(data_test['sequence-tokenized'], max_sequence_length = max_sequence_length)
         y_test = data_test['ccs'].to_numpy()
         np.save(f'./data/ionmod/test/{dataset}_x_test.npy', x_test)
         np.save(f'./data/ionmod/test/{dataset}_y_test.npy', y_test)
@@ -192,8 +200,7 @@ class data_ionmod():
         if ionmod_full:
             return cls.load_ionmod(dataset, ionmod_full=ionmod_full)
         else:
-            return cls.load_ionmod(dataset)
-        
+            return cls.load_ionmod(dataset)       
         
     @classmethod
     def load_testing_transformer(cls, dataset):
@@ -201,18 +208,130 @@ class data_ionmod():
         y_test = np.load(f'./data/ionmod/test/{dataset}_y_test.npy')
 
         return (x_test, y_test)
+
+class data_meier():
+
+    @classmethod
+    def split_data(cls, data, split = 0.8):
+        data = data.sample(frac=1, random_state=random).reset_index(drop=True)
+        split_index = int(len(data) * split)
+        return data[:split_index], data[split_index:]
+
+    @classmethod
+    def load_data(cls, dataset):
+        if dataset == 'small':
+            data = pd.read_csv(MEIER_DIR + '/meier_small.csv')
+        if dataset == 'train':
+            data = pd.read_csv(MEIER_DIR + '/SourceData_Figure_1.csv')
+        if dataset == 'meier':
+            datatrain = pd.read_csv(MEIER_DIR + '/SourceData_Figure_1.csv')
+            datatest = pd.read_csv(MEIER_DIR + '/SourceData_Figure_4.csv')
+            return datatrain, datatest
+
+        return cls.split_data(data=data, split=0.8)
     
+    @classmethod
+    def meier_to_integer(cls, sequences, max_sequence_length = 60):
+        
+        MEIER_ALPHABET = {
+            "A": 1,
+            "C": 2,
+            "D": 3,
+            "E": 4,
+            "F": 5,
+            "G": 6,
+            "H": 7,
+            "I": 8,
+            "K": 9,
+            "L": 10,
+            "M": 11,
+            "N": 12,
+            "P": 13,
+            "Q": 14,
+            "R": 15,
+            "S": 16,
+            "T": 17,
+            "V": 18,
+            "W": 19,
+            "Y": 20,
+            "1": 21, # M(ox)
+            "*": 22  # (ac)
+        }
+
+        array = np.zeros([len(sequences), max_sequence_length], dtype=int)
+        for i, sequence in enumerate(sequences):
+            # remove '_'
+            sequence = sequence[1:-1]
+            # if sequence starts with (ac) replace with '*'
+            if sequence.startswith('(a'):
+                sequence = '*' + sequence[4:]
+            sequence = re.sub('M\(ox\)', '1', sequence)
+            for j, s in enumerate(sequence):
+                array[i, j] = MEIER_ALPHABET[s]
+        return array
+    
+    @classmethod
+    def integer_to_sequence(cls, X):
+        int2seq = '_ACDEFGHIKLMNPQRSTVWY1*'
+        int2seqf = lambda x : ''.join([int2seq[c] for c in x if c > 0])
+        return ([int2seqf(x) for x in X])
+    
+    @classmethod
+    def load_meier(cls, dataset, max_sequence_length = 60):
+        data_train_val, data_test = cls.load_data(dataset)
+        
+        if dataset == 'train':
+            x_train = cls.meier_to_integer(data_train_val['Modified sequence'], max_sequence_length = max_sequence_length)
+            y_train = data_train_val['CCS'].to_numpy()
+            x_val = cls.meier_to_integer(data_test['Modified sequence'], max_sequence_length = max_sequence_length)
+            y_val = data_test['CCS'].to_numpy()
+
+            return (x_train, y_train), (x_val, y_val)
+        
+        data_train, data_val = cls.split_data(data_train_val, split=0.875)
+
+        x_train = cls.meier_to_integer(data_train['Modified sequence'], max_sequence_length = max_sequence_length)
+        y_train = data_train['CCS'].to_numpy()
+        x_val = cls.meier_to_integer(data_val['Modified sequence'], max_sequence_length = max_sequence_length)
+        y_val = data_val['CCS'].to_numpy()
+        
+        if dataset == 'meier':
+            x_test = cls.meier_to_integer(data_test['Modified_sequence'], max_sequence_length = max_sequence_length)
+            y_test = data_test['CCS'].to_numpy()
+        else:
+            x_test = cls.meier_to_integer(data_test['Modified sequence'], max_sequence_length = max_sequence_length)
+            y_test = data_test['CCS'].to_numpy()
+        
+        np.save(f'./data/meier_2021/test/{dataset}_x_test.npy', x_test)
+        np.save(f'./data/meier_2021/test/{dataset}_y_test.npy', y_test)
+
+        return (x_train, y_train), (x_val, y_val)
+
+    @classmethod
+    def load_training_transformer(cls, dataset):
+        return cls.load_meier(dataset)
+
+    @classmethod
+    def load_testing_transformer(cls, dataset):
+        if dataset == 'train': print("No test data for 'train' use 'meier' instead"); exit(0)
+        x_test = np.load(f'./data/meier_2021/test/{dataset}_x_test.npy')
+        y_test = np.load(f'./data/meier_2021/test/{dataset}_y_test.npy')
+
+        return (x_test, y_test)
+
 def load_test_data(data, input_file, seq_header, rt_header, CLS, seq_length):
     if input_file is not None:
         print('Incomplete')
         exit(0)
     elif data in IONMOD_DATASETS:
-        x_test, y_test = data_ionmod.load_testing_transformer(data)                
+        x_test, y_test = data_ionmod.load_testing_transformer(data)
+        all_peps = data_ionmod.integer_to_sequence_phospho(x_test)
+    elif data in MEIER_DATASETS:
+        x_test, y_test = data_meier.load_testing_transformer(data)
+        all_peps = data_meier.integer_to_sequence(x_test)               
     else:
         print('Unknown model')
         exit(0)
-
-    all_peps = data_ionmod.integer_to_sequence_phospho(x_test)
 
     print(seq_length, x_test.shape)
     assert seq_length >= x_test.shape[1]
@@ -230,6 +349,8 @@ class DatasetCCS():
                 (x_train, y_train), (x_val, y_val) = data_ionmod.load_training_transformer(dataset, ionmod_full)
             else:
                 (x_train, y_train), (x_val, y_val) = data_ionmod.load_training_transformer(dataset)
+        elif dataset in MEIER_DATASETS:
+            (x_train, y_train), (x_val, y_val) = data_meier.load_training_transformer(dataset)
         else: 
             print('Unknown data')
             exit(0)
